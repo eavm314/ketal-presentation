0cdaa206db07deb523efb59bc227a4dd03d516e5
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-12-08 16:22:27 -0400

    planificacion

--- src/pages/FasePlanificacion.jsx
+++ src/pages/FasePlanificacion.jsx
@@ -1,8 +1,5 @@
-
-
 function FasePlanificacion() {
-  return (
-
+    return (
         <div className="container mx-auto px-10 py-8 rounded-3xl bg-slate-50" style={{ backgroundColor: 'rgba(217, 217, 217, 0.74)' }}>
             <h1 className='text-2xl font-bold' >Fase de Planificación </h1>
             <p className="text-xl font-light text-left py-4 " >
@@ -26,26 +23,24 @@ function FasePlanificacion() {
                 <li>Fase de Cierre: Evaluación del proyecto, documentación y transición a operaciones.</li>
             </ul>
 
-
             <div className="text-center mb-9" >
 
-                <a href="https://www.notion.so/8d7c36374b024d45b6c7d54f73acb7ec?v=fe088133a4f2405486be9f65d05afca4" target="_blank" rel = "noreferrer">
+                <a href="https://www.notion.so/8d7c36374b024d45b6c7d54f73acb7ec?v=fe088133a4f2405486be9f65d05afca4" target="_blank" rel="noreferrer">
                     <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General del
                         Proyecto</h2>
                 </a>
 
-                <a href="https://www.notion.so/7fd2db96e764452388165ceff6d6308a?v=5aedbce3e42947b1b8a30b282959288d" target="_blank" rel = "noreferrer">
-                <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General Web
-                    Backend</h2>
+                <a href="https://www.notion.so/7fd2db96e764452388165ceff6d6308a?v=5aedbce3e42947b1b8a30b282959288d" target="_blank" rel="noreferrer">
+                    <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General Web
+                        Backend</h2>
                 </a>
 
-                <a href="https://www.notion.so/c7620716a92a4c7dbe088a606a6dc196?v=0664661cbb974175a67e8bc50e26f44a" target="_blank" rel = "noreferrer">
-                <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General Web
-                    Frontend</h2>
+                <a href="https://www.notion.so/c7620716a92a4c7dbe088a606a6dc196?v=0664661cbb974175a67e8bc50e26f44a" target="_blank" rel="noreferrer">
+                    <h2 className="text-2xl font-bold text-left text-primary uppercase py-4">Plan General Web
+                        Frontend</h2>
                 </a>
 
             </div>
-
 
             <h1 className='text-2xl font-bold' >Uso de herramientas </h1>
 
@@ -69,172 +64,215 @@ function FasePlanificacion() {
                 </li>
             </ul>
 
-            <br/>
+            <br />
             <div className="grid grid-cols-3 gap-4 justify-center items-center" >
                 <div className="text-center">
-                    <img src="https://th.bing.com/th/id/R.b6b42b0d08c8685ae401ce792a9b58fc?rik=57IEh7EFsxXmRA&pid=ImgRaw&r=0" alt="Logo de Notion" className="w-20 h-20 mx-auto"/>
+                    <img src="https://th.bing.com/th/id/R.b6b42b0d08c8685ae401ce792a9b58fc?rik=57IEh7EFsxXmRA&pid=ImgRaw&r=0" alt="Logo de Notion" className="w-20 h-20 mx-auto" />
                     <p>Notion</p>
                 </div>
                 <div className="text-center">
                     <img src="https://th.bing.com/th/id/OIP.akV9KjAtdaROFei8n8dnowHaHw?rs=1&pid=ImgDetMain"
-                         alt="Logo de Figma"
-                         className="w-20 h-20 mx-auto"/>
-                        <p>Figma</p>
+                        alt="Logo de Figma"
+                        className="w-20 h-20 mx-auto" />
+                    <p>Figma</p>
                 </div>
                 <div className="text-center">
                     <img
                         src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"
                         alt="Logo de React.js"
-                        className="w-20 h-20 mx-auto"/>
-                        <p>React.js</p>
+                        className="w-20 h-20 mx-auto" />
+                    <p>React.js</p>
                 </div>
                 <div className="text-center">
                     <img src="https://riseuplabs.com/wp-content/uploads/2021/04/express.js-icon.png"
-                         alt="Logo de Express.js"
-                         className="w-20 h-20 mx-auto"/>
-                        <p>Express.js</p>
+                        alt="Logo de Express.js"
+                        className="w-20 h-20 mx-auto" />
+                    <p>Express.js</p>
                 </div>
                 <div className="text-center">
                     <img
                         src="https://th.bing.com/th/id/R.cfa3a32613483a1ee48fac169aeb6359?rik=Lxp1uM0Qxn40kA&pid=ImgRaw&r=0"
-                        alt="Logo de Jest" className="w-20 h-20 mx-auto"/>
-                        <p>Jest</p>
+                        alt="Logo de Jest" className="w-20 h-20 mx-auto" />
+                    <p>Jest</p>
                 </div>
                 <div className="text-center">
                     <img
                         src="https://th.bing.com/th/id/R.a89c871423e0845750d27cf4ea910e53?rik=JvWxlJgKBtnUyQ&pid=ImgRaw&r=0"
                         alt="Logo de Cypress"
-                        className="w-20 h-20 mx-auto"/>
-                        <p>Cypress</p>
+                        className="w-20 h-20 mx-auto" />
+                    <p>Cypress</p>
                 </div>
                 <div className="text-center">
                     <img
                         src="https://th.bing.com/th/id/R.55b112b344a5cb45a6becf64bf5a5cf2?rik=sAaXelEMUzQDrw&pid=ImgRaw&r=0"
                         alt="Logo de Mocha JS"
-                        className="w-20 h-20 mx-auto"/>
+                        className="w-20 h-20 mx-auto" />
 
-                        <p>Mocha JS</p>
+                    <p>Mocha JS</p>
                 </div>
             </div>
-            <br/>
+            <br />
 
-            <a href="https://docs.google.com/document/d/1KGTHxPwsybFohuLlVXKgVeSX7r8Akobdpf8Fc6xkzxc/edit" target="_blank" rel = "noreferrer">
+            <a href="https://docs.google.com/document/d/14IfqHkI3DG0ZFzB7PJGzghdMyqs-6y79/edit" target="_blank" rel="noreferrer">
                 <h1 className='text-2xl font-bold' >Gestión de Riesgos</h1>
             </a>
 
+            <h3 className='text-xl font-bold'>Evaluación de Riesgos Clave</h3>
             <ul className="list-disc pl-6 text-xl font-light pb-4" >
-                <li>Identificación de Riesgos: Riesgos técnicos, de recursos y de tiempo evaluados en impacto y
-                    probabilidad.
-                </li>
-                <li>Análisis y Evaluación: Uso de una escala del 1 al 5 para evaluar probabilidad e impacto de cada
-                    riesgo.
-                </li>
-                <li>Estrategias de Mitigación: Desarrollo de planes de acción para cada riesgo identificado.</li>
-                <li>Plan de Contingencia: Medidas para enfrentar los riesgos, como tener equipos de desarrollo de
-                    respaldo.
-                </li>
-                <li>Monitoreo y Comunicación: Revisión periódica de los riesgos y comunicación constante con el equipo y
-                    los
-                    stakeholders.
+                <li><strong>Cambios en los Requisitos del Cliente:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li><strong>Probabilidad:</strong> Moderada</li>
+                        <li><strong>Impacto:</strong> Alto</li>
+                        <li><strong>Desafío:</strong> Cambios significativos en requisitos pueden alterar
+                            plazos y alcance</li>
+                    </ul>
                 </li>
 
-                <li>
-                    Riesgos Identificados y Estrategias de Mitigación: Lista de todos los riesgos identificados, junto a
-                    las estrategias necesarias para su mitigación.
+                <li><strong>Retrasos en la Disponibilidad de Datos:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li><strong>Probabilidad:</strong> Alta </li>
+                        <li><strong>Impacto:</strong> Moderado</li>
+                        <li><strong>Desafío:</strong> Entrega tardía de información clave puede afectar cronogramas</li>
+                    </ul>
                 </li>
-            </ul>
-            <br/>
 
-            <a href="https://docs.google.com/document/d/1KGTHxPwsybFohuLlVXKgVeSX7r8Akobdpf8Fc6xkzxc/edit" target="_blank" rel = "noreferrer">
-                <h1 className='text-2xl font-bold' >Análisis del Tamaño</h1>
-            </a>
-
-            <p className="text-xl font-light text-left py-4" >
-
-                En el análisis del tamaño del Sistema CRM de TIGO, se empleó la herramienta COCOMO,
-                un modelo de estimación de software que predice el esfuerzo de un producto basándose en parámetros como
-                tamaño, costo, tiempo y calidad. El modelo COCOMO clasifica proyectos en tres tipos: Orgánico,
-                semi-separado y embebido, cada uno representando la complejidad del proyecto, el tamaño del equipo y el
-                entendimiento del problema. Dado que el Sistema CRM de TIGO es considerado un proyecto de tamaño mediano,
-                se optó por el tipo de proyecto semi-separado y el modelo intermedio. Este último implica calcular la cantidad
-                de líneas de código en miles y considerar los Cost Drivers, que miden la experiencia, capacidad y fiabilidad del proyecto
-                y del equipo de trabajo. Para calcular el esfuerzo necesario para completar el proyecto, se aplican fórmulas
-                específicas definidas por el modelo intermedio para proyectos semi-separados.
-
-
-
-            </p>
-
-            <p className="text-xl font-light text-left py-4" >
-                Para un total de 16 meses, se va a necesitar un total de 13 personas para poder terminar el proyecto del CRM de TIGO.
-            </p>
-
-            <a href="https://docs.google.com/document/d/1KGTHxPwsybFohuLlVXKgVeSX7r8Akobdpf8Fc6xkzxc/edit" target="_blank" rel = "noreferrer">
-                <h1 className='text-2xl font-bold' >Análisis del Costo</h1>
-            </a>
-
-            <p className="text-xl font-light text-left py-4" >
-                Para poder estimar el costo del proyecto, se debe tomar en cuenta tres variables: complejidad, especialización y tiempo.
-
-                Para empezar, tomando en cuenta los salarios base para los desarrolladores por mes, basado en categoría:
-
-            </p>
-
-            <ul className="list-disc pl-6 text-xl font-light pb-4" >
-                <li>Junior: 3500 - 5000 Bs.
+                <li><strong>Problemas Técnicos en Hosting y Servidores:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li><strong>Probabilidad:</strong> Moderada </li>
+                        <li><strong>Impacto:</strong> Alto</li>
+                        <li><strong>Desafío:</strong> Fallos en infraestructura tecnológica pueden comprometer el rendimiento</li>
+                    </ul>
                 </li>
-                <li> Normal: 5000 - 7000 Bs.
+
+                <li><strong>Vulnerabilidades en la Seguridad:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li><strong>Probabilidad:</strong> Moderada  </li>
+                        <li><strong>Impacto:</strong> Muy alto</li>
+                        <li><strong>Desafío:</strong> Riesgo de ataques cibernéticos que comprometan datos sensibles</li>
+                    </ul>
                 </li>
-                <li>Senior: 7000 - 12000 Bs.</li>
+
+                <li><strong>Desalineación en Comunicación y Expectativas:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li><strong>Probabilidad:</strong> Moderada </li>
+                        <li><strong>Impacto:</strong> Moderado</li>
+                        <li><strong>Desafío:</strong>  Malentendidos podrían afectar alcance y prioridades</li>
+                    </ul>
+                </li>
+
+                <li><strong>Desafíos en la Adopción del Sistema:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li><strong>Probabilidad:</strong> Alta  </li>
+                        <li><strong>Impacto:</strong> Moderado</li>
+                        <li><strong>Desafío:</strong> Resistencia al cambio por parte del personal podría retrasar implementación.</li>
+                    </ul>
+                </li>
+
             </ul>
 
-            <p className="text-xl font-light text-left py-4" >
-                Esto brinda el análisis de complejidad, y para este caso, debido a la experiencia del equipo de desarrollo, se tomará 4000 Bs. como salario por mes para los desarrolladores (total de 5 desarrolladores).
-
-                Por otro lado, las especializaciones, son las siguientes:
-            </p>
-
+            <h3 className='text-xl font-bold'>Estrategias de Mitigación</h3>
             <ul className="list-disc pl-6 text-xl font-light pb-4" >
-                <li>Chatbot: 2000 Bs.
+                <li><strong>Cambios en los Requisitos del Cliente:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Reuniones regulares con responsables.</li>
+                        <li>Gestión de cambios basada en ISO 31000.</li>
+                    </ul>
                 </li>
-                <li> UI/UX: 1000 Bs.
+
+                <li><strong>Retrasos en la Disponibilidad de Datos:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Cronogramas claros con márgenes.</li>
+                        <li>Responsabilidades documentadas en herramientas como Trello.</li>
+                    </ul>
                 </li>
-                <li>FullStack: 2000 Bs.</li>
+
+                <li><strong>Problemas Técnicos en Hosting y Servidores:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Monitoreo constante según ISO/IEC 20000.</li>
+                        <li>Pruebas de carga y configuración de backups.</li>
+                    </ul>
+                </li>
+
+                <li><strong>Seguridad:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Diseño bajo ISO/IEC 27001.</li>
+                        <li>Auditorías y pruebas de penetración periódicas.</li>
+                    </ul>
+                </li>
+
+                <li><strong>Comunicación:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Punto de contacto único y herramientas como JIRA para seguimiento.</li>
+                    </ul>
+                </li>
+
+                <li><strong>Adopción del Sistema:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Programas de capacitación personalizados.  </li>
+                        <li>Canal de soporte técnico interno.</li>
+                    </ul>
+                </li>
+
             </ul>
 
-            <p className="list-disc pl-6 text-xl font-light pb-4" >
-
-                Debido a que un desarrollador se ocupará del Chatbot, otro del UI/UX, y los tres restantes del desarrollo FullStack.
-
-                Por lo tanto:
-
-            </p>
-
+            <h3 className='text-xl font-bold'>Planes de Contingencia</h3>
             <ul className="list-disc pl-6 text-xl font-light pb-4" >
-                <li> 4000 Bs. x 5
+                <li><strong>Cambios en los Requisitos del Cliente:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Cláusulas contractuales para costos y tiempos adicionales.</li>
+                    </ul>
                 </li>
-                <li> 20000 Bs. + 2000 Bs. + 1000 Bs. + 2000 Bs. + 2000 Bs. + 2000 Bs.
+
+                <li><strong>Retrasos en la Disponibilidad de Datos:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Avanzar en módulos independientes de la información faltante.</li>
+                    </ul>
                 </li>
+
+                <li><strong>Problemas Técnicos en Hosting y Servidores:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Entorno local de respaldo y planes de recuperación según ISO/IEC 22301.</li>
+                    </ul>
+                </li>
+
+                <li><strong>Seguridad:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Plan de respuesta a incidentes y seguros cibernéticos.</li>
+                    </ul>
+                </li>
+
+                <li><strong>Comunicación:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Reuniones extraordinarias para priorización de tareas.</li>
+                    </ul>
+                </li>
+
+                <li><strong>Adopción del Sistema:</strong>
+                    <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                        <li>Equipo interno para promover el uso del CRM y soporte inicial.
+                        </li>
+                    </ul>
+                </li>
+
             </ul>
 
-            <p className="list-disc pl-6 text-xl font-light pb-4" >En total: 29000 Bs. al mes, para el desarrollo del proyecto.</p>
+            <h3 className='text-xl font-bold'>Normativas Aplicadas</h3>
+            <ul className="list-disc pl-6 text-xl font-light pb-4" >
+                <li><strong>ISO 31000:</strong> Gestión de riesgos.</li>
 
+                <li><strong>ISO/IEC 20000:</strong> Gestión de servicios TI.</li>
 
+                <li><strong>ISO/IEC 27001:</strong>
+                    Seguridad de la información.
+                </li>
 
+                <li><strong>ISO/IEC 22301:</strong>
+                Continuidad del negocio.
+                </li>
 
-
-
-
-
-
-
-
-            <br/>
-
-
-
+            </ul>
         </div>
-  );
+    );
 }
 
 export default FasePlanificacion;
